27ca52504320254d6e0584607a99b280b3e193c0
JuanPabloRodriguezC <jprodriguezcano99@gmail.com>
2024-12-10 16:43:06 -0600

    class instantiation with date

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dataCollection import CryptoDataCollector
 #initialize collector
 collector = CryptoDataCollector()
 try:
-    collector.collect_data('BTCUSDT', '1h')
+    collector.collect_data('BTCUSDT', '1h', '2019-01-01')
 except KeyboardInterrupt:
     print("Collection stopped by user")
 
